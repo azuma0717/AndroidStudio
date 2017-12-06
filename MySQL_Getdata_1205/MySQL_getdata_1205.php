5e511c0051d3d041becb9681595affdb6a1cfbbc
azuma yuki <yuki@azumanoMacBook-Pro-2.local>
2017-12-06 20:49:32 +0530

    php_upload

---
+++ MySQL_Getdata_1205/MySQL_getdata_1205.php
@@ -0,0 +1,27 @@
+<?php
+
+    $conn = mysqli_connect('localhost','root','root','Android1st');
+    $query = 'select * from imageinfo1';
+    $result = mysqli_query($conn,$query);
+
+    $response = array();
+    if(mysqli_num_rows($result) > 0){
+
+      while ($row = mysqli_fetch_assoc($result)) {
+        array_push($response,$row);
+      }
+
+    }
+    else{
+
+        echo "Not find. Try again";
+    }
+
+    echo json_encode(array("data"=>$response));
+
+
+
+
+
+
+?>
